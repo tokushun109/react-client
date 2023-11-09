69e7cdcfac291a6fe08fad5bf8eb4c0b909572d8
tokushun109 <tokushun109@gmail.com>
2023-11-09 17:43:33 +0900

    #7 シェアボタンの大まかなレイアウトを作成

---
+++ src/components/molecules/ShareButtons/index.tsx
@@ -0,0 +1,32 @@
+import Icon from '@/components/atoms/Icon'
+import styles from './styles.module.scss'
+import ReplyIcon from '@mui/icons-material/Reply'
+import TwitterIcon from '@mui/icons-material/Twitter'
+import FacebookIcon from '@mui/icons-material/Facebook'
+
+const ShareButtons = () => {
+    return (
+        <div className={styles['container']}>
+            <div className={styles['message']}>
+                <div className={styles['reply']}>
+                    <ReplyIcon />
+                </div>
+                <div>Share This Page♪</div>
+            </div>
+            <div className={styles['icon-area']}>
+                <div>
+                    <Icon size={40}>
+                        <TwitterIcon />
+                    </Icon>
+                </div>
+                <div>
+                    <Icon size={40}>
+                        <FacebookIcon />
+                    </Icon>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ShareButtons
