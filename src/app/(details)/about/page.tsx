575cd420cafc3718a6c67e98953054dc801edbeb
tokushun109 <tokushun109@gmail.com>
2023-11-29 12:40:05 +0900

    #34 storyの画像を追加

--- src/app/(details)/about/page.tsx
+++ src/app/(details)/about/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import Section from '@/components/templates/Section'
 import { ColorEnum } from '@/types'
 
@@ -8,8 +10,12 @@ const About = () => {
         <div className={styles['container']}>
             <Section title="Story" button={false} color={ColorEnum.Secondary}>
                 <div className={styles['content']}>
-                    <div className={styles['content-part']}>画像</div>
-                    <div className={styles['content-part']}>文章</div>
+                    <div className={styles['column']}>
+                        <div className={styles['image-wrapper']}>
+                            <Image src="/image/about/story.jpg" fill alt="story" sizes="100%" style={{ objectFit: 'cover', borderRadius: 24 }} />
+                        </div>
+                    </div>
+                    <div className={styles['column']}>文章</div>
                 </div>
             </Section>
         </div>
