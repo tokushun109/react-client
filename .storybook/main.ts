f17b466f27328fca5b20c97893ce045ccb94fae2
tokushun109 <tokushun109@gmail.com>
2024-01-31 14:08:03 +0900

    #44 storybookの初期設定を大まかに追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,14 +1,43 @@
 import type { StorybookConfig } from '@storybook/nextjs'
+import path from 'path'
 
 const config: StorybookConfig = {
-    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-onboarding', '@storybook/addon-interactions'],
+    stories: ['./Configure.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-onboarding',
+        '@storybook/addon-interactions',
+        '@storybook/addon-styling',
+    ],
     framework: {
         name: '@storybook/nextjs',
         options: {},
     },
     docs: {
         autodocs: 'tag',
+    },
+    webpackFinal: async (config) => {
+        if (config.resolve) {
+            config.resolve.alias = {
+                ...config.resolve.alias,
+                '@': path.resolve(__dirname, '../src'),
+            }
+        }
+        if (config.module && config.module.rules) {
+            config.module.rules.push({
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        options: {
+                            loader: 'sass-loader',
+                            prependData: '@import "@/styles/variables.scss";@import "@/styles/mixins.scss";',
+                        },
+                    },
+                ],
+            })
+        }
+        return config
     },
 }
 export default config
